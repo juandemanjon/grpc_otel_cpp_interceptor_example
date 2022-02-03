8084d5eb84f6538a76ae8ca2cf7550f8eb7b2b62
jmanjon <juande.manjon@gmail.com>
2022-02-02 23:03:00 -0800

    Basic gRPC server

---
+++ scripts/fast-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Before running this script, make sure $VCPKG_ROOT is set, e.g.
+# VCPKG_ROOT=/vcpkg && export VCPKG_ROOT
+
+cd ..
+rm -rf build/
+cmake -G Ninja -D CMAKE_BUILD_TYPE=Release -D ENABLE_TESTING:BOOL=FALSE -D ENABLE_IPO=ON -S . -B build
+cmake --build build
+cd build || exit
+ctest --output-on-failure -j2
